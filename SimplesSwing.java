169126524f487fcb58ff57184008e581789ef0ee
ChepeISC <jomacarrilloib@ittepic.edu.mx>
2016-02-02 21:09:34 -0600

    PruebaNn

---
+++ SimplesSwing.java
@@ -0,0 +1,17 @@
+import javax.swing.java
+
+	public class SimplestSwing{
+	public static void main(String[] args){
+
+JFrame frame = new JFrame("Simplest Swing");
+
+frame.setVisible(true);
+}
+
+
+
+
+
+
+
+}
